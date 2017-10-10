5431f4aae83e60ea158ed9256d70653deedabaa0
Anton Carlsson <mail@antoncarlsson.se>
2017-10-10 16:46:20 +0200

    Forgot to update the Game.py file for the UI.

--- Game.py
+++ Game.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 from GameEngine.Player import Player
 from GamePlatform.Platform import Platform
+from GameUI.UI import UI
 
 if __name__ == '__main__':
-    p1 = Player("Anton", "ai", "hard")
-    p2 = Player("Tor", "user")
-
-    p = Platform(p1, p2)
-    p.startGame()
+    game = UI()
+    game.startGame()

0e3fbb6a0a6f4f9e456947e399bf2a7ef7ac9470
Lowe Eklund <lowe@art-is.se>
2017-10-10 15:04:34 +0200

    Fixed bugs, refactored and added some comments

--- Tournament.py
+++ Tournament.py
@@ -10,6 +10,10 @@ class Tournament:
         self.final = [] 
         self.difficulties = ['easy', 'medium', 'hard']     
 
+    """
+        Runs the quarterfinals and adds the winner to the semifinals.
+        If a game ends in a tie the winner is choosen at random with equal probability. 
+    """
     def runQuarterfinals(self):
         # matches are querterFinals[0] against quarterFinals[1]
         # qurterFinals[2] agains quarterFinals[3] and so on.
@@ -18,37 +22,43 @@ class Tournament:
             # collect the winner of each quarter final and semiFinals.append(winner)
             winner = p.startGame()
             if (winner == 'tie'):
-                self.semifinals.append[self.quarterfinals[x + random.randint(0,1)]]
+                self.semifinals.append(self.quarterfinals[x + random.randint(0,1)])
             else: 
                 self.semifinals.append(winner)
     
+    """
+        Runs the semifinals and adds the winner to the final.
+        If a game ends in a tie the winner is choosen at random with equal probability. 
+    """
     def runSemifinals(self):
         # start matches semiFinals[0] against semiFinals[1] ...
         for x in [0,2]:
             p = Platform(self.semifinals[x], self.semifinals[x + 1])
             winner = p.startGame()
             if (winner == 'tie'):
-                self.final.append[self.semifinals[x + random.randint(0,1)]]
+                self.final.append(self.semifinals[x + random.randint(0,1)])
             else: 
                 self.final.append(winner)
     
+    """
+        Runs the final.
+        If a game ends in a tie the winner is choosen at random with equal probability. 
+    """
     def runFinal(self):
         p = Platform(self.final[0], self.final[1])
         winner = p.startGame()
         if (winner == 'tie'):
-            winner = final[random.randint(0, 1)]
+            winner = self.final[random.randint(0, 1)]
 
-        print("The winner is..." + winner.name + "!")
+        print("The winner of the tournament is " + winner.name + "!")
 
+    """
+        Creates Player objects given the name and difficulties entered by the user.
+    """
     def fillTournament(self): 
-        inp = input("Enter the number of players in the tournament ('4' or '8'): ")
-        while (inp not in ['4', '8']):
-            inp = input("Invalid input. Enter the number of players in the tournament ('4' or '8'): ")
-        self.numberOfPlayers = int(inp)
-        # Entering players 
         players = self.enterPlayers()
         ais = self.enterAIs(self.numberOfPlayers - len(players))
-        # fill quarterFinals array with players and randomize the order
+
         if (self.numberOfPlayers == 8):
             self.fillQuarterfinals(players, ais)
         if (self.numberOfPlayers == 4):
@@ -68,13 +78,27 @@ class Tournament:
             self.semifinals.append(Player(ai[0], "ai", ai[1]))
         random.shuffle(self.semifinals)
 
+    """
+        Prompts the user for the tournament size. Saves it to self.numberOfPlayers.
+    """
+    def getTournamentSize(self):
+        inp = input("Enter the number of players in the tournament ('4' or '8'): ")
+        while (inp not in ['4', '8']):
+            inp = input("Invalid input. Enter the number of players in the tournament ('4' or '8'): ")
+        self.numberOfPlayers = int(inp)
+
     def startTournament(self):
+        self.getTournamentSize()
         self.fillTournament()
         if (self.numberOfPlayers == 8):
             self.runQuarterfinals()
         self.runSemifinals()
         self.runFinal()
 
+    """
+        :returns: The name of the players entered by the user.
+        :rtype: [String]
+    """
     def enterPlayers(self):
         entersPlayers = True 
         players = []
@@ -86,6 +110,10 @@ class Tournament:
                 entersPlayers = False
         return players
 
+    """
+        :returns: The name of the ai and the difficulty entered by the user (name, difficulty).
+        :rtype: [(String, String)]
+    """
     def enterAIs(self, amount):
         ais = []
         for x in range(0, amount):
@@ -97,8 +125,3 @@ class Tournament:
             ais.append((name, difficulty))
         return ais
 
-
-
-#if __name__ == '__main__': 
-#    t = Tournament()
-#    t.startTournament()
